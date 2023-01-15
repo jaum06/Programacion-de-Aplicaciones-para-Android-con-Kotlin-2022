f63587d00323c9678e05cb3b27fc0d7197cc4b59
Andres <j.a.u.m.06@hotmail.com>
2023-01-14 21:01:33 -0500

    Operadores de calculo

--- app/src/main/java/com/zlasher/cursobasico/MainActivity.kt
+++ app/src/main/java/com/zlasher/cursobasico/MainActivity.kt
@@ -25,5 +25,35 @@ class MainActivity : AppCompatActivity() {
 
         nombre = "Andres"
         println(saludo)
+
+        var a = 5 + 5
+        var b = 10 - 2
+        var c = 3 * 4
+        var d = 10 / 5
+        var e = 10 % 3
+        var f = 10 / 6
+
+        var aPreIncremento = 5
+        var bPreDecremento = 5
+        var cPostIncremento = 5
+        var dPostDecremento = 5
+
+        println(aPreIncremento)
+        println(++aPreIncremento) //Incrementa primero, luego regresa. Salida 6
+        println(aPreIncremento)
+
+        println(bPreDecremento)
+        println(--bPreDecremento) //Primero decrementa, luego regresa. Salida 4
+        println(bPreDecremento)
+
+        println(cPostIncremento)
+        println(cPostIncremento++) //Primero regresa, luego incrementa. Salida 5
+        println(cPostIncremento)
+
+        println(dPostDecremento)
+        println(dPostDecremento--) //Primero regresa, luego decrementa. Salida 5
+        println(dPostDecremento)
+
+        saldo += sueldo
     }
 }
