28a8df0dd9aacd64a6f497e85d9606220ea7a495
Andres <j.a.u.m.06@hotmail.com>
2023-01-15 17:42:19 -0500

    Break

--- app/src/main/java/com/zlasher/cursobasico/MainActivity.kt
+++ app/src/main/java/com/zlasher/cursobasico/MainActivity.kt
@@ -55,11 +55,12 @@ class MainActivity : AppCompatActivity() {
 
         var pin = 1234
         var intentos = 0
-        var clave_ingresada = 1230
+        var clave_ingresada = 1233
 
         do {
             println("Ingrese el PIN")
             println("Clave ingresada: ${clave_ingresada++}")
+            if (clave_ingresada == pin) break
             intentos++
         } while (intentos < 3 && clave_ingresada != pin)
 
