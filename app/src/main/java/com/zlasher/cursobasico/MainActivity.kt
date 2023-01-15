a4e9a293620f21c267b3147c86fa99c1c25e46e8
Andres <j.a.u.m.06@hotmail.com>
2023-01-14 20:14:36 -0500

    Variables, valores y constantes

--- app/src/main/java/com/zlasher/cursobasico/MainActivity.kt
+++ app/src/main/java/com/zlasher/cursobasico/MainActivity.kt
@@ -4,8 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+
+        const val moneda = "EUR"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fecha = "14/01/2023"
+        var nombre = "Juan"
+
+        nombre = "Andres"
+        println(nombre)
     }
 }
