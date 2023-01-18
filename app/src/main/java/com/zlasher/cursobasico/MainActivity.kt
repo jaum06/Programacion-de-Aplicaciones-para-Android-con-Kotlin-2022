99819c3ccc51ae3d65a73c57430553374a139d64
Andres <j.a.u.m.06@hotmail.com>
2023-01-18 11:39:25 -0500

    2. POO implementación visual del código

--- app/src/main/java/com/zlasher/cursobasico/MainActivity.kt
+++ app/src/main/java/com/zlasher/cursobasico/MainActivity.kt
@@ -2,32 +2,47 @@ package com.zlasher.cursobasico
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var pokemon: Pokemon
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var num: Int
-        val jota: Person = Person("Andres", "sad13r")
-        var anonimo: Person = Person()
-        println(jota.alive)
-        println(jota.name)
-        println(jota.passport)
-
-        anonimo.Person()
-        println(anonimo.alive)
-        println(anonimo.name)
-        println(anonimo.passport)
-
-        jota.die()
-        println(jota.alive)
-
-        val bicho: Pokemon = Pokemon()
-        println(bicho.getName())
-        println(bicho.getAttackPower())
-        println(bicho.setLife(30f))
-        println(bicho.getLife())
     }
+
+    fun createNewPokemon(v: View) {
+
+        val etName = findViewById<EditText>(R.id.etName)
+        val etAttackPower = findViewById<EditText>(R.id.etAttackPower)
+        pokemon = Pokemon()
+
+        if (!etName.text.isNullOrEmpty() && !etAttackPower.text.isNullOrEmpty()) {
+            pokemon.Pokemon(etName.text.toString(), etAttackPower.text.toString().toFloat())
+        }
+
+        val ivPokemon = findViewById<ImageView>(R.id.ivPokemon)
+        ivPokemon.setImageResource(R.mipmap.pokemon)
+
+        val tvPokemon = findViewById<TextView>(R.id.tvPokemon)
+        loadDataPokemon(tvPokemon, pokemon)
+    }
+
+    private fun loadDataPokemon(tvPokemon: TextView, pokemon: Pokemon) {
+
+        var description: String = ""
+        description = pokemon.getName()
+        description += " (AP: " + pokemon.getAttackPower().toInt()
+        description += " - L:" + pokemon.getLife().toInt() + ")"
+
+        tvPokemon.text = description
+    }
+
+
 }
