cb78aa17a869f5bc2fabdc2f538662585b93d39b
Andres <j.a.u.m.06@hotmail.com>
2023-01-19 16:33:47 -0500

    2. POO Colaboración entre clases

--- app/src/main/java/com/zlasher/cursobasico/FirePokemon.kt
+++ app/src/main/java/com/zlasher/cursobasico/FirePokemon.kt
@@ -4,6 +4,8 @@ class FirePokemon(name: String = "Pok", attackPower: Float = 30f, life: Float = 
     Pokemon(name, attackPower, life) {
 
     private var ballTemperature: Int = 90
+    lateinit var ball: ballFire
+    var numBall: Int = 0
 
     fun FirePokemon(name: String, attackPower: Float, ballTemperature: Int) {
         this.name = name
@@ -15,6 +17,16 @@ class FirePokemon(name: String = "Pok", attackPower: Float = 30f, life: Float = 
 
     override fun attack() {
         //Toast.makeText(mainContext, "Ataque con bola de fuego!!!", Toast.LENGTH_LONG).show()
+        super.attack()
         println("Ataque con bola de fuego!!!")
+        println("Bola ${++numBall}")
+        ball = ballFire(ballTemperature)
+        ball.throwBall()
+    }
+}
+
+class ballFire(var temperature: Int = 100) {
+    fun throwBall() {
+        println("Tirando bola con temperatura de $temperature")
     }
 }
