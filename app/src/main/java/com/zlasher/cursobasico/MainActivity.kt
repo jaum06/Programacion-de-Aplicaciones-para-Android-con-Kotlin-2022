d7e3b91736bfe5ef1f33dcd5b93fe3034f133b41
Andres <j.a.u.m.06@hotmail.com>
2023-01-31 13:25:22 -0500

    4.5 juego: Limitaciones de movimiento del usuario

--- app/src/main/java/com/zlasher/cursobasico/MainActivity.kt
+++ app/src/main/java/com/zlasher/cursobasico/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.zlasher.cursobasico
 
-import android.graphics.Point
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.TypedValue
 import android.view.View
 import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TableRow
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 
@@ -15,12 +11,49 @@ class MainActivity : AppCompatActivity() {
 
     private var cellSelectedX = 0
     private var cellSelectedY = 0
+    private lateinit var board: Array<IntArray>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initScreenGame()
+        resetBoard()
         setFirstPosition()
+    }
+
+    fun checkCellClicked(v: View) {
+
+        val name = v.tag.toString()
+        val x = name.subSequence(1, 2).toString().toInt()
+        val y = name.subSequence(2, 3).toString().toInt()
+
+        checkCell(x, y)
+    }
+
+    private fun checkCell(x: Int, y: Int) {
+
+        val difX = kotlin.math.abs(x - cellSelectedX)
+        val difY = kotlin.math.abs(y - cellSelectedY)
+
+        var checkTrue = false
+
+        if (board[x][y] == 1) checkTrue = false
+        else if (difX == 1 && difY == 2) checkTrue = true
+        else if (difX == 2 && difY == 1) checkTrue = true
+
+        if (checkTrue) selectCell(x, y)
+    }
+
+    private fun resetBoard() {
+
+        /*
+            0 -> Esta libre
+            1 -> Casilla marcada
+            2 -> Es un bonus
+            9 -> Es una opción del movimiento actual
+        */
+
+        board = Array(8) { IntArray(8) { 0 } }
     }
 
     private fun setFirstPosition() {
@@ -30,13 +63,16 @@ class MainActivity : AppCompatActivity() {
 
         cellSelectedX = x
         cellSelectedY = y
-        selectCell(x, y, "previous_cell")
-        selectCell(x, y, "selected_cell")
+        selectCell(x, y)
     }
 
-    private fun selectCell(x: Int, y: Int, color: String) {
+    private fun selectCell(x: Int, y: Int) {
 
-        paintHorseCell(x, y, color)
+        board[x][y] = 1
+        paintHorseCell(cellSelectedX, cellSelectedY, "previous_cell")
+        cellSelectedX = x
+        cellSelectedY = y
+        paintHorseCell(x, y, "selected_cell")
     }
 
     private fun paintHorseCell(x: Int, y: Int, color: String) {
@@ -62,6 +98,7 @@ class MainActivity : AppCompatActivity() {
         val clmessage = findViewById<ConstraintLayout>(R.id.clmessage)
         clmessage.visibility = View.INVISIBLE
     }
+
 
     /*private fun setSizeBoard() {
 
