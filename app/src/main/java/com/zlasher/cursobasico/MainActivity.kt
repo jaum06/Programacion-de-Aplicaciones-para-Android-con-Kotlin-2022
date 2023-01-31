86f339d2cfd26e92f37a6df38cf5a72cd5589a70
Andres <j.a.u.m.06@hotmail.com>
2023-01-31 11:30:51 -0500

    4.3 juego: Redimensionar elementos y quitar action bar

--- app/src/main/java/com/zlasher/cursobasico/MainActivity.kt
+++ app/src/main/java/com/zlasher/cursobasico/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.zlasher.cursobasico
 
+import android.graphics.Point
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TableRow
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +16,49 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initScreenGame()
+    }
+
+    private fun initScreenGame() {
+
+        setSizeBoard()
+        hideMessage()
+    }
+
+    private fun hideMessage() {
+
+        val clmessage = findViewById<LinearLayout>(R.id.clmessage)
+        clmessage.visibility = View.INVISIBLE
+    }
+
+    private fun setSizeBoard() {
+
+        var imageView: ImageView
+        val display = windowManager.defaultDisplay
+        val size = Point()
+        display.getSize(size)
+        val width = size.x
+        val widthDp = (width / resources.displayMetrics.density)
+        val lateralMarginsDp = 10
+        val widthCell = ((widthDp - lateralMarginsDp) / 8)
+        val heightCell = widthCell
+
+        for (i in 0..7) {
+            for (j in 0..7) {
+                imageView = findViewById(resources.getIdentifier("c$i$j", "id", packageName))
+                val height = TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    heightCell,
+                    resources.displayMetrics
+                ).toInt()
+                val width = TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    heightCell,
+                    resources.displayMetrics
+                ).toInt()
+                imageView.layoutParams(TableRow.LayoutParams(width, height))
+            }
+        }
     }
 }
 
