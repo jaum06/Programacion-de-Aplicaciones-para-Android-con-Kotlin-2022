9743b6a33431f5b1f0b3e92fb79c2805aed9ed94
Andres <j.a.u.m.06@hotmail.com>
2023-01-31 12:21:31 -0500

    4.4 juego: Posición aleatoria

--- app/src/main/java/com/zlasher/cursobasico/MainActivity.kt
+++ app/src/main/java/com/zlasher/cursobasico/MainActivity.kt
@@ -8,30 +8,62 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TableRow
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
-
+    private var cellSelectedX = 0
+    private var cellSelectedY = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initScreenGame()
+        setFirstPosition()
+    }
+
+    private fun setFirstPosition() {
+
+        val x = (0..7).random()
+        val y = (0..7).random()
+
+        cellSelectedX = x
+        cellSelectedY = y
+        selectCell(x, y, "previous_cell")
+        selectCell(x, y, "selected_cell")
+    }
+
+    private fun selectCell(x: Int, y: Int, color: String) {
+
+        paintHorseCell(x, y, color)
+    }
+
+    private fun paintHorseCell(x: Int, y: Int, color: String) {
+
+        val imageView: ImageView = findViewById(resources.getIdentifier("c$x$y", "id", packageName))
+        imageView.setBackgroundColor(
+            ContextCompat.getColor(
+                this,
+                resources.getIdentifier(color, "color", packageName)
+            )
+        )
+        imageView.setImageResource(R.drawable.ic_horse)
     }
 
     private fun initScreenGame() {
 
-        setSizeBoard()
         hideMessage()
+        //setSizeBoard()
     }
 
     private fun hideMessage() {
 
-        val clmessage = findViewById<LinearLayout>(R.id.clmessage)
+        val clmessage = findViewById<ConstraintLayout>(R.id.clmessage)
         clmessage.visibility = View.INVISIBLE
     }
 
-    private fun setSizeBoard() {
+    /*private fun setSizeBoard() {
 
         var imageView: ImageView
         val display = windowManager.defaultDisplay
@@ -59,7 +91,7 @@ class MainActivity : AppCompatActivity() {
                 imageView.layoutParams(TableRow.LayoutParams(width, height))
             }
         }
-    }
+    }*/
 }
 
 
