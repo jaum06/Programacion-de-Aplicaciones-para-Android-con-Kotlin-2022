7f3c873b8e2ee5dce42c41b0dccd05ee4f388bb5
Andres <j.a.u.m.06@hotmail.com>
2023-05-08 16:34:00 -0500

    5.7.app deporte control de flujo de activities en inicio de sesión

--- app/src/main/java/com/zlasher/cursobasico/TermsActivity.kt
+++ app/src/main/java/com/zlasher/cursobasico/TermsActivity.kt
@@ -7,5 +7,9 @@ class TermsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_terms)
+
+        /*val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_terms)
+        setSupportActionBar(toolbar)
+        toolbar.title=getString(R.string.tvTerminosYCondiciones)*/
     }
 }
