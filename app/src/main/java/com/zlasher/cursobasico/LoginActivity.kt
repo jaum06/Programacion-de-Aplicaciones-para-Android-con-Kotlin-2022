11566bfcb699a50b06e58f74e409fe159cfd51bc
Andres <j.a.u.m.06@hotmail.com>
2023-05-08 12:50:43 -0500

    5.3.app deporte login

--- app/src/main/java/com/zlasher/cursobasico/LoginActivity.kt
+++ app/src/main/java/com/zlasher/cursobasico/LoginActivity.kt
@@ -1,11 +1,70 @@
 package com.zlasher.cursobasico
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.LinearLayout
+import com.google.firebase.auth.FirebaseAuth
+import kotlin.properties.Delegates
 
 class LoginActivity : AppCompatActivity() {
+
+    companion object {
+        lateinit var userEmail: String
+        lateinit var providerSession: String
+    }
+
+    private var email by Delegates.notNull<String>()
+    private var password by Delegates.notNull<String>()
+    private lateinit var etEmail: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var lyTerms: LinearLayout
+
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        lyTerms = findViewById(R.id.lyTerminosYCondiciones)
+        lyTerms.visibility = View.INVISIBLE
+
+        etEmail = findViewById(R.id.etEmail)
+        etPassword = findViewById(R.id.etPassword)
+        mAuth = FirebaseAuth.getInstance()
+    }
+
+    fun login(view: View) {
+        loginUser()
+    }
+
+    private fun loginUser() {
+
+        email = etEmail.text.toString()
+        password = etPassword.text.toString()
+
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) goHome(email, "email")
+                else {
+                    if (lyTerms.visibility == View.INVISIBLE) lyTerms.visibility = View.VISIBLE
+                    else {
+                        val cbAcept = findViewById<CheckBox>(R.id.cbTerminosYCondiciones)
+                        if (cbAcept.isChecked) register()
+                    }
+                }
+            }
+    }
+
+    private fun goHome(email: String, provider: String) {
+
+        userEmail = email
+        providerSession = provider
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
